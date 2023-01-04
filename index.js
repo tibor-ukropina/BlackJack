2d8f6df27f1281cc181deee362078ac6dac58ec0
Tibor Ukropina <77939216+tibor-ukropina@users.noreply.github.com>
2023-01-03 21:41:40 -0500

    Update index.js
    updated let to const

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+const messageEl = document.getElementById("message-el")
+const sumEl = document.getElementById("sum-el")
+const cardsEl = document.getElementById("cards-el")
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
